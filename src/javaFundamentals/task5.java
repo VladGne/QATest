108d676a5775ea2935763cf6a9475028d244d63f
Vlad Gneushev <guinwow@gmail.com>
2018-11-09 18:11:59 +0400

    Add checking for zero disk number

--- src/javaFundamentals/task5.java
+++ src/javaFundamentals/task5.java
@@ -3,26 +3,30 @@ package javaFundamentals;
 import java.util.Scanner;
 
 public class task5 {
+    // Task 5 - Tower of Honoi
+
     public static void main(String args[]){
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter disk number");
         int diskNumber = scanner.nextInt();
-        char A='A', B='B', C='C';
+        char[] sticks = {'A', 'B', 'C'};
 
-        solveTower(diskNumber, A, C, B);
+        solveTower(diskNumber,sticks[0], sticks[2], sticks[1]);
     }
 
     private static void solveTower(int diskNumber, char A, char C, char B){
 
-        if(diskNumber == 1){
-            System.out.println(String.format("Move disk %d: %c -> %c ", diskNumber, A, C));
-        }
+        if (diskNumber >0){
+            if(diskNumber == 1){
+                System.out.println(String.format("Move disk %d: %c -> %c ", diskNumber, A, C));
+            }
 
-        else {
-            solveTower(diskNumber-1, A, B, C);
-            System.out.println(String.format("Move disk %d: %c -> %c ", diskNumber, A, C));
-            solveTower(diskNumber-1, B, C, A);
+            else {
+                solveTower(diskNumber-1, A, B, C);
+                System.out.println(String.format("Move disk %d: %c -> %c ", diskNumber, A, C));
+                solveTower(diskNumber-1, B, C, A);
+            }
         }
     }
 }
