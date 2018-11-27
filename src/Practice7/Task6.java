2e5596e4c4a3417cd71c6d91ca8cd660411cfad2
Vlad Gneushev <guinwow@gmail.com>
2018-11-27 21:30:16 +0400

    [...] updated regex

--- src/Practice7/Task6.java
+++ src/Practice7/Task6.java
@@ -4,9 +4,9 @@ public class Task6 {
 
     public static void main(String args[]){
 
-        String[] testCases = new String[]{"2012","2013", "2014", "2500", "3003", "3500", "4004", "5001", "5042", "5043", "5044"};
+        String[] testCases = new String[]{"2012","2013", "2014", "2500", "3003", "3500", "4004", "5001", "5042", "5043", "5000"};
 
-        String pattern = "(.)\\1+";
+        String pattern = "(.)\\1";
         String result;
 
         for (String testCase : testCases){
