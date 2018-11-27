9abcc546924da52ef324614b28e7427abba8f74f
Vlad Gneushev <guinwow@gmail.com>
2018-11-27 13:40:59 +0400

    [...] updated pattern

--- src/Practice7/Task4.java
+++ src/Practice7/Task4.java
@@ -9,13 +9,13 @@ public class Task4 {
 
         String[] testCases = new String[]{"12/12/2012", "31/22/2013", "44/04/2014", "21/55/2500"};
 
-        String pattern = "(([0-2][0-9]|3[0-1])/(0[1-9]|1[1-2])/([1-9][0-9][0-9][0-9]))";
+        String regex = "((0[1-9]|[1-2][0-9]|3[0-1])/(0[1-9]|1[1-2])/([1-9][0-9][0-9][0-9]))";
 
-        Pattern p = Pattern.compile(pattern);
+        Pattern pattern = Pattern.compile(regex);
 
         for (String testCase : testCases) {
-            Matcher m = p.matcher(testCase);
-            System.out.print(testCase + " - " + m.matches() + "\n");
+            Matcher matcher = pattern.matcher(testCase);
+            System.out.print(testCase + " - " + matcher.matches() + "\n");
         }
     }
 }
